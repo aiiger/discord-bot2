7dea8c3dbb5abcc811e9f6eabf961d431f48b6ce
Ali Mohammadi <3ali.mohammadi@gmail.com>
2024-10-31 17:58:16 +0400

    lewlewlewl

--- auth.js
+++ auth.js
@@ -7,11 +7,10 @@ const port = 3000;
 const CLIENT_ID = process.env.FACEIT_CLIENT_ID;
 const CLIENT_SECRET = process.env.FACEIT_CLIENT_SECRET;
 const REDIRECT_URI = 'http://localhost:3000/callback';
-const AUTHORIZATION_URL = 'https://api.faceit.com/auth/v1/oauth/authorize';
 const TOKEN_URL = 'https://api.faceit.com/auth/v1/oauth/token';
 
-app.get('/login', (req, res) => {
 app.get('/login', (_, res) => {
+    const authUrl = `${AUTHORIZATION_URL}?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
     res.redirect(authUrl);
 });
 
