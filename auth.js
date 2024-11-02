d607f518cd33e3a963510d4480c8d3e7a7b66b6a
Ali Mohammadi <3ali.mohammadi@gmail.com>
2024-11-02 06:10:28 +0400

    Add additional OAuth scopes for enhanced functionality

--- auth.js
+++ auth.js
@@ -50,7 +50,7 @@ function getAuthUrl() {
         response_type: 'code',
         client_id: FACEIT_CLIENT_ID,
         redirect_uri: REDIRECT_URI,
-        scope: 'openid profile email chat.messages.read chat.messages.write'
+        scope: 'openid profile email chat.messages.read chat.messages.write chat.rooms.read chat.rooms.write matches matches:read matches:write tournaments tournaments:read tournaments:write hubs hubs:read hubs:write'
     });
     
     const url = `${AUTH_URL}?${params.toString()}`;
