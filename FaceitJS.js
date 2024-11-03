5d4899e1ddabe6d9e59249bc2168a01b4f9ee02e
Ali Mohammadi <3ali.mohammadi@gmail.com>
2024-11-04 03:59:54 +0400

    es

--- FaceitJS.js
+++ FaceitJS.js
@@ -1,145 +1,140 @@
-// #region imports
-//***** IMPORTS *****//
+// FaceitJS.js
 
-/* CHAMPIONSHIPS */
-const getChampionships = require('./endpoints/championships/getChampionships.js');
-const getChampionshipsById = require('./endpoints/championships/getChampionshipsById.js');
-const getChampionshipsMatches = require('./endpoints/championships/getChampionshipsMatches.js');
-const getChampionshipsSubscriptions = require('./endpoints/championships/getChampionshipsSubscriptions.js');
-const getChampionshipsResults = require('./endpoints/championships/getChampionshipsResults.js');
+// ***** IMPORTS ***** //
 
-/* GAMES */
-const getGames = require('./endpoints/games/getGames.js');
-const getGamesMatchmakings = require('./endpoints/games/getGamesMatchmakings.js');
-const getGamesById = require('./endpoints/games/getGamesById.js');
-const getGamesParent = require('./endpoints/games/getGamesParent.js');
-const getGamesQueues = require('./endpoints/games/getGamesQueues.js');
-const getGamesQueuesById = require('./endpoints/games/getGamesQueuesById.js');
-const getGamesQueuesByIdBans = require('./endpoints/games/getGamesQueuesByIdBans.js');
-const getGamesQueuesByIdByRegion = require('./endpoints/games/getGamesQueuesByIdByRegion.js');
+// CHAMPIONSHIPS
+import getChampionships from './endpoints/championships/getChampionships.js';
+import getChampionshipsById from './endpoints/championships/getChampionshipsById.js';
+import getChampionshipsMatches from './endpoints/championships/getChampionshipsMatches.js';
+import getChampionshipsSubscriptions from './endpoints/championships/getChampionshipsSubscriptions.js';
+import getChampionshipsResults from './endpoints/championships/getChampionshipsResults.js';
 
-/* HUBS */
-const getHubsById = require('./endpoints/hubs/getHubsById.js');
-const getHubsMatches = require('./endpoints/hubs/getHubsMatches.js');
-const getHubsMembers = require('./endpoints/hubs/getHubsMembers');
-const getHubsRoles = require('./endpoints/hubs/getHubsRoles.js');
-const getHubsRules = require('./endpoints/hubs/getHubsRules.js');
-const getHubsStats = require('./endpoints/hubs/getHubsStats.js');
+// GAMES
+import getGames from './endpoints/games/getGames.js';
+import getGamesMatchmakings from './endpoints/games/getGamesMatchmakings.js';
+import getGamesById from './endpoints/games/getGamesById.js';
+import getGamesParent from './endpoints/games/getGamesParent.js';
+import getGamesQueues from './endpoints/games/getGamesQueues.js';
+import getGamesQueuesById from './endpoints/games/getGamesQueuesById.js';
+import getGamesQueuesByIdBans from './endpoints/games/getGamesQueuesByIdBans.js';
+import getGamesQueuesByIdByRegion from './endpoints/games/getGamesQueuesByIdByRegion.js';
 
-/* LEADERBOARDS */
-const getLeaderboardChampionships = require('./endpoints/leaderboards/getLeaderboardChampionships.js');
-const getLeaderboardChampionshipsByGroup = require('./endpoints/leaderboards/getLeaderboardChampionshipsByGroup.js');
-const getLeaderboardByHub = require('./endpoints/leaderboards/getLeaderboardByHub.js');
-const getLeaderboardByHubGeneral = require('./endpoints/leaderboards/getLeaderboardByHubGeneral.js');
-const getLeaderboardByHubBySeason = require('./endpoints/leaderboards/getLeaderboardByHubBySeason.js');
-const getLeaderboardById = require('./endpoints/leaderboards/getLeaderboardById.js');
-const getLeaderboardByIdByPlayer = require('./endpoints/leaderboards/getLeaderboardByIdByPlayer.js');
+// HUBS
+import getHubsById from './endpoints/hubs/getHubsById.js';
+import getHubsMatches from './endpoints/hubs/getHubsMatches.js';
+import getHubsMembers from './endpoints/hubs/getHubsMembers.js';
+import getHubsRoles from './endpoints/hubs/getHubsRoles.js';
+import getHubsRules from './endpoints/hubs/getHubsRules.js';
+import getHubsStats from './endpoints/hubs/getHubsStats.js';
 
-/* LEAGUES */
-const getLeaguesById = require('./endpoints/leagues/getLeaguesById.js');
-const getLeaguesBySeasonId = require('./endpoints/leagues/getLeaguesBySeasonId.js');
-const getLeaguesByPlayersId = require('./endpoints/leagues/getLeaguesByPlayersId.js');
+// LEADERBOARDS
+import getLeaderboardChampionships from './endpoints/leaderboards/getLeaderboardChampionships.js';
+import getLeaderboardChampionshipsByGroup from './endpoints/leaderboards/getLeaderboardChampionshipsByGroup.js';
+import getLeaderboardByHub from './endpoints/leaderboards/getLeaderboardByHub.js';
+import getLeaderboardByHubGeneral from './endpoints/leaderboards/getLeaderboardByHubGeneral.js';
+import getLeaderboardByHubBySeason from './endpoints/leaderboards/getLeaderboardByHubBySeason.js';
+import getLeaderboardById from './endpoints/leaderboards/getLeaderboardById.js';
+import getLeaderboardByIdByPlayer from './endpoints/leaderboards/getLeaderboardByIdByPlayer.js';
 
-/* MATCHES */
-const getMatchesById = require('./endpoints/matches/getMatchesById.js');
-const getMatchesStats = require('./endpoints/matches/getMatchesStats.js');
+// LEAGUES
+import getLeaguesById from './endpoints/leagues/getLeaguesById.js';
+import getLeaguesBySeasonId from './endpoints/leagues/getLeaguesBySeasonId.js';
+import getLeaguesByPlayersId from './endpoints/leagues/getLeaguesByPlayersId.js';
 
-/* MATCHMAKINGS */
-const getMatchmakingsById = require('./endpoints/matchmakings/getMatchmakingsById.js');
+// MATCHES
+import getMatchesById from './endpoints/matches/getMatchesById.js';
+import getMatchesStats from './endpoints/matches/getMatchesStats.js';
 
-/* ORGANIZERS */
-const getOrganizers = require('./endpoints/organizers/getOrganizers.js');
-const getOrganizersById = require('./endpoints/organizers/getOrganizersById.js');
-const getOrganizersByIdChampionships = require('./endpoints/organizers/getOrganizersByIdChampionships.js');
-const getOrganizersByIdGames = require('./endpoints/organizers/getOrganizersByIdGames.js');
-const getOrganizersByIdHubs = require('./endpoints/organizers/getOrganizersByIdHubs.js');
-const getOrganizersByIdTournaments = require('./endpoints/organizers/getOrganizersByIdTournaments.js');
+// MATCHMAKINGS
+import getMatchmakingsById from './endpoints/matchmakings/getMatchmakingsById.js';
 
-/* PLAYERS */
-const getPlayer = require('./endpoints/players/getPlayers.js');
-const getPlayerById = require('./endpoints/players/getPlayersById.js');
-const getPlayerHistory = require('./endpoints/players/getPlayerHistory.js');
-const getPlayerHubs = require('./endpoints/players/getPlayerHubs.js');
-const getPlayerStats = require('./endpoints/players/getPlayerStats.js');
-const getPlayerTeams = require('./endpoints/players/getPlayerTeams.js');
-const getPlayerTournaments = require('./endpoints/players/getPlayerTournaments.js');
+// ORGANIZERS
+import getOrganizers from './endpoints/organizers/getOrganizers.js';
+import getOrganizersById from './endpoints/organizers/getOrganizersById.js';
+import getOrganizersByIdChampionships from './endpoints/organizers/getOrganizersByIdChampionships.js';
+import getOrganizersByIdGames from './endpoints/organizers/getOrganizersByIdGames.js';
+import getOrganizersByIdHubs from './endpoints/organizers/getOrganizersByIdHubs.js';
+import getOrganizersByIdTournaments from './endpoints/organizers/getOrganizersByIdTournaments.js';
 
-/* RANKINGS */
-const getRanking = require('./endpoints/rankings/getRanking.js');
-const getPositionRanking = require('./endpoints/rankings/getPositionInRanking.js');
+// PLAYERS
+import getPlayer from './endpoints/players/getPlayers.js';
+import getPlayerById from './endpoints/players/getPlayersById.js';
+import getPlayerHistory from './endpoints/players/getPlayerHistory.js';
+import getPlayerHubs from './endpoints/players/getPlayerHubs.js';
+import getPlayerStats from './endpoints/players/getPlayerStats.js';
+import getPlayerTeams from './endpoints/players/getPlayerTeams.js';
+import getPlayerTournaments from './endpoints/players/getPlayerTournaments.js';
 
-/* SEARCH */
-const getSearchChampionships = require('./endpoints/search/getSearchChampionships.js');
-const getSearchHubs = require('./endpoints/search/getSearchHubs.js');
-const getSearchOrganizers = require('./endpoints/search/getSearchOrganizers.js');
-const getSearchPlayers = require('./endpoints/search/getSearchPlayers.js');
-const getSearchTeams = require('./endpoints/search/getSearchTeams.js');
-const getSearchTournaments = require('./endpoints/search/getSearchTournaments.js');
+// RANKINGS
+import getRanking from './endpoints/rankings/getRanking.js';
+import getPositionRanking from './endpoints/rankings/getPositionInRanking.js';
 
-/* TEAMS */
-const getTeamsById = require('./endpoints/teams/getTeamsById.js');
-const getTeamsStats = require('./endpoints/teams/getTeamsStats.js');
-const getTeamsTournaments = require('./endpoints/teams/getTeamsTournaments.js');
+// SEARCH
+import getSearchChampionships from './endpoints/search/getSearchChampionships.js';
+import getSearchHubs from './endpoints/search/getSearchHubs.js';
+import getSearchOrganizers from './endpoints/search/getSearchOrganizers.js';
+import getSearchPlayers from './endpoints/search/getSearchPlayers.js';
+import getSearchTeams from './endpoints/search/getSearchTeams.js';
+import getSearchTournaments from './endpoints/search/getSearchTournaments.js';
 
-/* TOURNMENTS */
-const getTournaments = require('./endpoints/tournaments/getTournaments.js');
-const getTournamentsById = require('./endpoints/tournaments/getTournamentsById.js');
-const getTournamentsByIdBrackets = require('./endpoints/tournaments/getTournamentsByIdBrackets.js');
-const getTournamentsByIdMatches = require('./endpoints/tournaments/getTournamentsByIdMatches.js');
-const getTournamentsByIdTeams = require('./endpoints/tournaments/getTournamentsByIdTeams.js');
+// TEAMS
+import getTeamsById from './endpoints/teams/getTeamsById.js';
+import getTeamsStats from './endpoints/teams/getTeamsStats.js';
+import getTeamsTournaments from './endpoints/teams/getTeamsTournaments.js';
 
-/* CHAT API */
-/* ROOM */
-const getRoomById = require('./endpoints/rooms/getRoomById.js');
-const getRoomByIdMessages = require('./endpoints/rooms/getRoomByIdMessages.js');
-const postRoomByIdMessage = require('./endpoints/rooms/postRoomByIdMessage.js');
-// #endregion
+// TOURNMENTS
+import getTournaments from './endpoints/tournaments/getTournaments.js';
+import getTournamentsById from './endpoints/tournaments/getTournamentsById.js';
+import getTournamentsByIdBrackets from './endpoints/tournaments/getTournamentsByIdBrackets.js';
+import getTournamentsByIdMatches from './endpoints/tournaments/getTournamentsByIdMatches.js';
+import getTournamentsByIdTeams from './endpoints/tournaments/getTournamentsByIdTeams.js';
 
-//***** CLASS *****//
-class FaceitJS{
-    /* #apiKeyServer;
-    #apiKeyClient; */
+// CHAT API
+// ROOM
+import getRoomById from './endpoints/rooms/getRoomById.js';
+import getRoomByIdMessages from './endpoints/rooms/getRoomByIdMessages.js';
+import postRoomByIdMessage from './endpoints/rooms/postRoomByIdMessage.js';
 
-    /* CONSTRUCTOR */
-    constructor(apiKeyServerSide, apiKeyClientSide){
+// ***** FACEITJS CLASS ***** //
+
+class FaceitJS {
+    constructor(apiKeyServerSide, apiKeyClientSide) {
         this.apiKeyServer = apiKeyServerSide;
         this.apiKeyClient = apiKeyClientSide;
     }
 
-    /* HEADERS FOR API REQUESTS */
     headers = {
         headers: {
-          accept: "application/json",
-          Authorization: `Bearer ` + this.apiKeyServer,
+            accept: "application/json",
+            Authorization: `Bearer ${this.apiKeyServer}`,
         },
-      };
+    };
 
-    /* METHODS */
-    getApiKeyServer(){
+    // Getter and Setter Methods
+    getApiKeyServer() {
         return this.apiKeyServer;
     }
 
-    setApiKeyServer(apiKeyServerSide){
+    setApiKeyServer(apiKeyServerSide) {
         this.apiKeyServer = apiKeyServerSide;
     }
 
-    getApiKeyClient(){
+    getApiKeyClient() {
         return this.apiKeyClient;
     }
 
-    setApiKeyClient(apiKeyClientSide){
+    setApiKeyClient(apiKeyClientSide) {
         this.apiKeyClient = apiKeyClientSide;
     }
 
-    getHeader(){
+    getHeader() {
         return this.headers;
     }
 }
 
-// #region prototype
-//***** PROTOTYPE *****//
+// ***** ADD PROTOTYPE METHODS ***** //
 
-/* CHAMPIONSHIPS */
+// CHAMPIONSHIPS
 FaceitJS.prototype.getChampionships = getChampionships;
 FaceitJS.prototype.championships = getChampionships;
 
@@ -157,7 +152,7 @@ FaceitJS.prototype.championshipsResults = getChampionshipsResults;
 FaceitJS.prototype.getChampionshipsSubscriptions = getChampionshipsSubscriptions;
 FaceitJS.prototype.championshipsSubscriptions = getChampionshipsSubscriptions;
 
-/* GAMES */
+// GAMES
 FaceitJS.prototype.getGames = getGames;
 FaceitJS.prototype.games = getGames;
 
@@ -185,7 +180,7 @@ FaceitJS.prototype.getGamesQueuesByIdByRegion = getGamesQueuesByIdByRegion;
 FaceitJS.prototype.gamesQueuesByIdByRegion = getGamesQueuesByIdByRegion;
 FaceitJS.prototype.gamesQueuesByRegion = getGamesQueuesByIdByRegion;
 
-/* HUBS */
+// HUBS
 FaceitJS.prototype.getHubsById = getHubsById;
 FaceitJS.prototype.getHubById = getHubsById;
 FaceitJS.prototype.hubsById = getHubsById;
@@ -206,7 +201,7 @@ FaceitJS.prototype.hubsRules = getHubsRules;
 FaceitJS.prototype.getHubsStats = getHubsStats;
 FaceitJS.prototype.hubsStats = getHubsStats;
 
-/* LEADERBOARDS */
+// LEADERBOARDS
 FaceitJS.prototype.getLeaderboardChampionships = getLeaderboardChampionships;
 FaceitJS.prototype.leaderboardChampionships = getLeaderboardChampionships;
 
@@ -236,7 +231,7 @@ FaceitJS.prototype.leaderboardByIdByPlayer = getLeaderboardByIdByPlayer;
 FaceitJS.prototype.getLeaderboardByPlayer = getLeaderboardByIdByPlayer;
 FaceitJS.prototype.leaderboardByPlayer = getLeaderboardByIdByPlayer;
 
-/* LEAGUES */
+// LEAGUES
 FaceitJS.prototype.getLeaguesById = getLeaguesById;
 FaceitJS.prototype.getLeagueById = getLeaguesById;
 FaceitJS.prototype.leaguesById = getLeaguesById;
@@ -252,7 +247,7 @@ FaceitJS.prototype.getLeagueByPlayersId = getLeaguesByPlayersId;
 FaceitJS.prototype.leaguesByPlayersId = getLeaguesByPlayersId;
 FaceitJS.prototype.leagueByPlayersId = getLeaguesByPlayersId;
 
-/* MATCHES */
+// MATCHES
 FaceitJS.prototype.getMatchesById = getMatchesById;
 FaceitJS.prototype.getMatchById = getMatchesById;
 FaceitJS.prototype.matchesById = getMatchesById;
@@ -261,11 +256,11 @@ FaceitJS.prototype.matchById = getMatchesById;
 FaceitJS.prototype.getMatchesStats = getMatchesStats;
 FaceitJS.prototype.matchesStats = getMatchesStats;
 
-/* MATCHMAKINGS */
+// MATCHMAKINGS
 FaceitJS.prototype.getMatchmakingsById = getMatchmakingsById;
 FaceitJS.prototype.matchmakingsById = getMatchmakingsById;
 
-/* ORGANIZERS */
+// ORGANIZERS
 FaceitJS.prototype.getOrganizers = getOrganizers;
 FaceitJS.prototype.organizers = getOrganizers;
 
@@ -284,7 +279,7 @@ FaceitJS.prototype.irganizersByIdHubs = getOrganizersByIdHubs;
 FaceitJS.prototype.getOrganizersByIdTournaments = getOrganizersByIdTournaments;
 FaceitJS.prototype.organizersByIdTournaments = getOrganizersByIdTournaments;
 
-/* PLAYERS */
+// PLAYERS
 FaceitJS.prototype.getPlayer = getPlayer;
 FaceitJS.prototype.player = getPlayer;
 
@@ -306,14 +301,14 @@ FaceitJS.prototype.playerTeams = getPlayerTeams;
 FaceitJS.prototype.getPlayerTournaments = getPlayerTournaments;
 FaceitJS.prototype.playerTournaments = getPlayerTournaments;
 
-/* RANKINGS */
+// RANKINGS
 FaceitJS.prototype.getRanking = getRanking;
 FaceitJS.prototype.ranking = getRanking;
 
 FaceitJS.prototype.getPositionRanking = getPositionRanking;
 FaceitJS.prototype.getPositionPlayer = getPositionRanking;
 
-/* SEARCH */
+// SEARCH
 FaceitJS.prototype.getSearchChampionships = getSearchChampionships;
 FaceitJS.prototype.searchChampionships = getSearchChampionships;
 
@@ -332,7 +327,7 @@ FaceitJS.prototype.searchTeams = getSearchTeams;
 FaceitJS.prototype.getSearchTournaments = getSearchTournaments;
 FaceitJS.prototype.searchTournaments = getSearchTournaments;
 
-/* TEAMS */
+// TEAMS
 FaceitJS.prototype.getTeamsById = getTeamsById;
 FaceitJS.prototype.getTeamById = getTeamsById;
 FaceitJS.prototype.teamsById = getTeamsById;
@@ -344,7 +339,7 @@ FaceitJS.prototype.teamsStats = getTeamsStats;
 FaceitJS.prototype.getTeamsTournaments = getTeamsTournaments;
 FaceitJS.prototype.teamsTournaments = getTeamsTournaments;
 
-/* TOURNMENTS */
+// TOURNMENTS
 FaceitJS.prototype.getTournaments = getTournaments;
 FaceitJS.prototype.tournaments = getTournaments;
 
@@ -368,8 +363,8 @@ FaceitJS.prototype.ournamentsByIdTeams = getTournamentsByIdTeams;
 FaceitJS.prototype.getTournamentsTeams = getTournamentsByIdTeams;
 FaceitJS.prototype.ournamentsTeams = getTournamentsByIdTeams;
 
-/* CHAT API */
-/* ROOM */
+// CHAT API
+// ROOM
 FaceitJS.prototype.getRoomById = getRoomById;
 FaceitJS.prototype.roomById = getRoomById;
 
@@ -382,7 +377,7 @@ FaceitJS.prototype.postRoomMessage = postRoomByIdMessage;
 FaceitJS.prototype.roomMessage = postRoomByIdMessage;
 FaceitJS.prototype.postMessage = postRoomByIdMessage;
 FaceitJS.prototype.message = postRoomByIdMessage;
-// #endregion
 
-//***** EXPORT *****//
-module.exports = FaceitJS;
+// ***** EXPORT THE FACEITJS CLASS ***** //
+
+export default FaceitJS;
