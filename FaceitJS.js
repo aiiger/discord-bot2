58be71dfb8e9126e72d18bd21268913790702a11
Ali <3ali.mohammadi@gmail.com>
2024-11-06 11:38:50 +0400

    meowcat

--- FaceitJS.js
+++ FaceitJS.js
@@ -5,19 +5,21 @@ function FaceitJS() {
   // Your constructor implementation here
 }
 
-function getChampionshipsById(id) {
+FaceitJS.prototype.getChampionshipsById = function(id) {
   // Your implementation here
-}
+};
 
-function getHubsById(id) {
+FaceitJS.prototype.getHubsById = function(id) {
   // Your implementation here
-}
+};
 
-FaceitJS.prototype.championshipsById = getChampionshipsById;
+FaceitJS.prototype.rehostChampionship = async function(eventId, gameId) {
+  // Your implementation here
+};
 
-// HUBS
-FaceitJS.prototype.getHubsById = getHubsById;
-FaceitJS.prototype.hubsById = getHubsById;
+FaceitJS.prototype.cancelChampionship = async function(eventId) {
+  // Your implementation here
+};
 
 // ***** EXPORT THE FACEITJS CLASS ***** //
 module.exports = FaceitJS;
@@ -34,68 +36,10 @@ module.exports.getAuthorizationUrl = function(state) {
   return `https://www.faceit.com/oauth/authorize?${params.toString()}`;
 };
 
-    // ***** CHAMPIONSHIP ACTIONS ***** //
+module.exports.getAccessTokenFromCode = async function(code) {
+  // Your implementation here
+};
 
-    /**
-     * Rehost a championship event.
-     * @param {string} eventId - The ID of the championship event.
-     * @param {string} gameId - The ID of the game.
-     * @returns {Promise<Object>} - The response data from the API.
-     */
-    async rehostChampionship(eventId, gameId) {
-        try {
-            const url = `https://api.faceit.com/championships/v1/events/${eventId}/rehost`;
-            const data = { gameId };
-            const headers = this.getHeader();
-            const response = await axios.post(url, data, headers);
-            return response.data;
-        } catch (error) {
-            logger.error(`Error rehosting championship: ${error.response?.data || error.message}`);
-            throw error;
-        }
-    }
-
-    /**
-     * Cancel a championship event.
-     * @param {string} eventId - The ID of the championship event.
-     * @returns {Promise<Object>} - The response data from the API.
-     */
-    async cancelChampionship(eventId) {
-        try {
-            const url = `https://api.faceit.com/championships/v1/events/${eventId}/cancel`;
-            const headers = this.getHeader();
-            const response = await axios.post(url, {}, headers);
-            return response.data;
-        } catch (error) {
-            logger.error(`Error canceling championship: ${error.response?.data || error.message}`);
-            throw error;
-        }
-    }
-}
-
-// ***** ADD PROTOTYPE METHODS ***** //
-
-// CHAMPIONSHIPS
-FaceitJS.prototype.getChampionshipsById = getChampionshipsById;
-FaceitJS.prototype.championshipsById = getChampionshipsById;
-
-// HUBS
-FaceitJS.prototype.getHubsById = getHubsById;
-FaceitJS.prototype.hubsById = getHubsById;
-
-// ***** EXPORT THE FACEITJS CLASS ***** //
-module.exports = FaceitJS;
-
-// ***** ADDITIONAL EXPORTS ***** //
-module.exports.getAuthorizationUrl = function(state) {
-    const params = new URLSearchParams({
-        response_type: 'code',
-        client_id: process.env.FACEIT_CLIENT_ID,
-        redirect_uri: process.env.REDIRECT_URI,
-        scope: 'openid profile email',
-        state: state,
-    });
-
-    // Use the correct OAuth2 authorization endpoint
-    return `https://api.faceit.com/auth/v1/oauth/authorize?${params.toString()}`;
+module.exports.getUserInfo = async function(accessToken) {
+  // Your implementation here
 };
