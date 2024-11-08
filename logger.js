78d9eb03bb993c919be5406d1545af436b1b722e
Ali <3ali.mohammadi@gmail.com>
2024-11-08 04:59:33 +0400

    logging

--- logger.js
+++ logger.js
@@ -3,15 +3,14 @@ const { createLogger, format, transports } = require('winston');
 const logger = createLogger({
     level: 'info',
     format: format.combine(
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+        format.timestamp(),
+        format.json()
     ),
     transports: [
         new transports.Console(),
-        // Add other transports like file if needed
-    ],
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' })
+    ]
 });
 
 module.exports = logger;
