819cba38e67af39980d27c95a4007c2b8d693984
Ali <3ali.mohammadi@gmail.com>
2024-11-12 22:25:00 +0400

    Reverted to accounts endpoint with PKCE

--- auth.js
+++ auth.js
@@ -32,7 +32,7 @@ const config = {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     redirectUri: process.env.REDIRECT_URI || 'https://faceit-bot-test-ae3e65bcedb3.herokuapp.com/callback',
-    authEndpoint: 'https://api.faceit.com/auth/v1/oauth/authorize',  // Changed to API endpoint
+    authEndpoint: 'https://accounts.faceit.com/accounts',  // Changed back to accounts endpoint
     tokenEndpoint: 'https://api.faceit.com/auth/v1/oauth/token',
     userInfoEndpoint: 'https://api.faceit.com/auth/v1/resources/userinfo'
 };
